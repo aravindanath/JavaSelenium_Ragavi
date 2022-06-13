d2a380bcbaa3cbb27b66fb5a887da904fdccf1ff
aravindanath <20963786+aravindanath@users.noreply.github.com>
2022-06-13 15:46:26 +0530

    first commit

---
+++ src/test/java/day1/DataTypes.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int age = 23; // 4bytes
+        long aaddhar = 234524524234l;
+        short pin = 23412;
+        float weight = 76.34f;
+        double rate = 2345234.2345234;
+        boolean wfh = false;
+        char ch = '%';
+
+        String name = "Arind";
+
+        System.out.println("Age: "+age);
+
+
+
+    }
+}
---
+++ src/test/java/day1/Operator_1.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Operator_1 {
+
+    public static void main(String[] args) {
+
+        float n1 = 23; // 4bytes
+
+        float n2 = 20.3f;
+
+        float sum = n1 +n2;
+        System.out.println(sum);
+/**
+ *  +
+ *  -
+ *  *
+ *  /
+ *  %
+ */
+
+
+    }
+}
